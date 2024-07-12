c82329caf197cf92bf561c6d521b8bf0513c342a
Salah Zanati <118008173+salah-zanati@users.noreply.github.com>
2024-07-12 17:59:41 +0400

    Update DialogAction compnent

--- src/Components/M3Dialog/M3DialogActions.tsx
+++ src/Components/M3Dialog/M3DialogActions.tsx
@@ -1,13 +1,22 @@
-import { DialogActions, DialogActionsProps } from '@mui/material'
-import React from 'react'
-interface M3DialogActions extends DialogActionsProps{
-    children: React.ReactNode,
-    disableSpacing:boolean
-}
-const M3DialogActions = ({children , disableSpacing} : M3DialogActions) => {
-  return (
-    <DialogActions disableSpacing={disableSpacing}>{children}</DialogActions>
-  )
+import { DialogActions, DialogActionsProps } from "@mui/material";
+import React from "react";
+
+// Define additional props and modified props for M3Card
+interface M3DialogActionModifiedProps {
+  disableSpacing: boolean;
 }
 
-export default M3DialogActions
+// Merge the new/ props with CardProps
+export type M3DialogActionProps = M3DialogActionModifiedProps &
+  DialogActionsProps;
+
+const M3DialogActions = (props: M3DialogActionProps) => {
+  const { children, disableSpacing } = props;
+  return (
+    <DialogActions {...props} disableSpacing={disableSpacing}>
+      {children}
+    </DialogActions>
+  );
+};
+
+export default M3DialogActions;
